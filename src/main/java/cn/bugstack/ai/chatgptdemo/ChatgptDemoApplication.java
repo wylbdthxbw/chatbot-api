ace8926ce219cb4f3b94696de67d4e8172ed789a
bl_msi <783605099@qq.com>
2024-07-29 13:06:14 +0800

    src

---
+++ src/main/java/cn/bugstack/ai/chatgptdemo/ChatgptDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.bugstack.ai.chatgptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/bugstack/ai/chatgptdemo/ChatgptDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bugstack.ai.chatgptdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
